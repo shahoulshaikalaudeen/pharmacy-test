4a3fd8016b2995786913ba25913db582942cd5e7
Your Name <shahoul95@gmail.com>
2026-06-25 23:40:22 +0200

    refacto file pharmacy.js

--- pharmacy.js
+++ pharmacy.js
@@ -9,6 +9,13 @@ export class Drug {
 export class Pharmacy {
   constructor(drugs = []) {
     this.drugs = drugs;
+
+    this.updaters = {
+      "Herbal Tea": this.updateHerbalTea.bind(this),
+      Fervex: this.updateFervex.bind(this),
+      Dafalgan: this.updateDafalgan.bind(this),
+      "Magic Pill": () => {},
+    };
   }
 
   increaseBenefit(drug, amount = 1) {
@@ -20,26 +27,15 @@ export class Pharmacy {
   }
 
   updateNormalDrug(drug) {
-    this.decreaseBenefit(drug, 1);
-
-    if (drug.expiresIn <= 0) {
-      this.decreaseBenefit(drug, 1);
-    }
+    this.decreaseBenefit(drug, drug.expiresIn <= 0 ? 2 : 1);
   }
 
   updateDafalgan(drug) {
-    this.decreaseBenefit(drug, 2);
-
-    if (drug.expiresIn <= 0) {
-      this.decreaseBenefit(drug, 2);
-    }
+    this.decreaseBenefit(drug, drug.expiresIn <= 0 ? 4 : 2);
   }
 
   updateHerbalTea(drug) {
-    this.increaseBenefit(
-      drug,
-      drug.expiresIn <= 0 ? 2 : 1
-    );
+    this.increaseBenefit(drug, drug.expiresIn <= 0 ? 2 : 1);
   }
 
   updateFervex(drug) {
@@ -50,34 +46,22 @@ export class Pharmacy {
 
     let increase = 1;
 
-    if (drug.expiresIn <= 10) {
-      increase += 1;
-    }
-
-    if (drug.expiresIn <= 5) {
-      increase += 1;
-    }
+    if (drug.expiresIn <= 10) increase++;
+    if (drug.expiresIn <= 5) increase++;
 
     this.increaseBenefit(drug, increase);
   }
 
   updateBenefitValue() {
     this.drugs.forEach((drug) => {
-      if (drug.name === "Magic Pill") {
-        return;
-      }
+      const update =
+        this.updaters[drug.name] ?? this.updateNormalDrug.bind(this);
 
-      if (drug.name === "Herbal Tea") {
-        this.updateHerbalTea(drug);
-      } else if (drug.name === "Fervex") {
-        this.updateFervex(drug);
-      } else if (drug.name === "Dafalgan") {
-        this.updateDafalgan(drug);
-      } else {
-        this.updateNormalDrug(drug);
-      }
+      update(drug);
 
-      drug.expiresIn--;
+      if (drug.name !== "Magic Pill") {
+        drug.expiresIn--;
+      }
     });
 
     return this.drugs;
